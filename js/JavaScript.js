74f36e5de084f862afb07564348edee2910eb59a
Fabio-Garcia27 <fabio@unipar.br>
2025-05-05 21:47:38 -0300

    first commit

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,3 +1,4 @@
+<!-- Fábio 05/05/2024 -->
 $(document).ready(function() {
     $('#add-experiencia').click(function() {
         $('#experiencias-container').append('<input type="text" name="experiencia[]" title="Descreva uma experiência"><br>');
@@ -26,46 +27,3 @@ $(document).ready(function() {
         });
     });
 });
-/*
-function preencherEstados() {
-    const estados = [
-        { sigla: 'AC', nome: 'Acre' },
-        { sigla: 'AL', nome: 'Alagoas' },
-        { sigla: 'AP', nome: 'Amapá' },
-        { sigla: 'AM', nome: 'Amazonas' },
-        { sigla: 'BA', nome: 'Bahia' },
-        { sigla: 'CE', nome: 'Ceará' },
-        { sigla: 'DF', nome: 'Distrito Federal' },
-        { sigla: 'ES', nome: 'Espírito Santo' },
-        { sigla: 'GO', nome: 'Goiás' },
-        { sigla: 'MA', nome: 'Maranhão' },
-        { sigla: 'MG', nome: 'Minas Gerais' },
-        { sigla: 'MS', nome: 'Mato Grosso do Sul' },
-        { sigla: 'MT', nome: 'Mato Grosso' },
-        { sigla: 'PA', nome: 'Pará' },
-        { sigla: 'PB', nome: 'Paraíba' },
-        { sigla: 'PE', nome: 'Pernambuco' },
-        { sigla: 'PI', nome: 'Piauí' },
-        { sigla: 'PR', nome: 'Paraná' },
-        { sigla: 'RJ', nome: 'Rio de Janeiro' },
-        { sigla: 'RN', nome: 'Rio Grande do Norte' },
-        { sigla: 'RO', nome: 'Rondônia' },
-        { sigla: 'RR', nome: 'Roraima' },
-        { sigla: 'RS', nome: 'Rio Grande do Sul' },
-        { sigla: 'SC', nome: 'Santa Catarina' },
-        { sigla: 'SE', nome: 'Sergipe' },
-        { sigla: 'SP', nome: 'São Paulo' },
-        { sigla: 'TO', nome: 'Tocantins' },
-    ];
-
-    const select = document.getElementById('estados');
-    estados.forEach((estado) => {
-        const option = document.createElement('option');
-        option.value = estado.sigla;
-        option.text = estado.nome;
-        select.appendChild(option);
-    });
-}
-
-document.addEventListener('DOMContentLoaded', preencherEstados);
- */
